1f23892bd3b2c92d93b2f31d091900ae33628fb4
tyshbyr <tyshbyr@gmail.com>
2023-02-05 03:19:35 +0500

    Добавлен скрипт для переноса данных

--- sqlite_to_postgres/load_data.py
+++ sqlite_to_postgres/load_data.py
@@ -1,20 +1,23 @@
 import sqlite3
-
 import psycopg2
 from psycopg2.extensions import connection as _connection
 from psycopg2.extras import DictCursor
 
+import settings
+from script import SQLiteExtractor, PostgresSaver, conn_context
+
 
 def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
     """Основной метод загрузки данных из SQLite в Postgres"""
-    # postgres_saver = PostgresSaver(pg_conn)
-    # sqlite_extractor = SQLiteExtractor(connection)
+    postgres_saver = PostgresSaver(pg_conn, settings.PG_TABLE)
+    sqlite_extractor = SQLiteExtractor(connection, settings.SQLITE_TABLE, settings.LIMIT)
 
-    # data = sqlite_extractor.extract_movies()
-    # postgres_saver.save_all_data(data)
+    data = sqlite_extractor.extract_movies()
+    postgres_saver.save_all_data(data)
 
 
 if __name__ == '__main__':
     dsl = {'dbname': 'movies_database', 'user': 'app', 'password': '123qwe', 'host': '127.0.0.1', 'port': 5432}
-    with sqlite3.connect('db.sqlite') as sqlite_conn, psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
-        load_from_sqlite(sqlite_conn, pg_conn)
+    with conn_context(settings.SQLITE_FILE) as sqlite_conn, psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
+            load_from_sqlite(sqlite_conn, pg_conn)
+ 
---
+++ sqlite_to_postgres/script.py
@@ -0,0 +1,55 @@
+import sqlite3
+import io
+from dataclasses import dataclass, field
+from psycopg2.extensions import connection as _connection
+from contextlib import contextmanager
+
+
+@dataclass
+class TableColumns:
+    table: str
+    columns: str
+    
+
+@contextmanager
+def conn_context(db_path: str):
+    conn = sqlite3.connect(f"file:{db_path}?mode=rw", uri=True)
+    yield conn
+    conn.close()
+
+
+@dataclass
+class SQLiteExtractor:
+    conn: sqlite3.Connection
+    sqlite_table: TableColumns
+    limit: int = field(default=100)
+    
+    def extract_movies(self):
+        curs = self.conn.cursor()
+        curs.arraysize = self.limit
+        curs.execute(f"SELECT {self.sqlite_table.columns} FROM {self.sqlite_table.table};")
+        while True:
+            data = curs.fetchmany()
+            if not data:
+                break
+            yield data
+    
+
+@dataclass    
+class PostgresSaver:
+    pg_conn: _connection
+    pg_table: TableColumns
+    
+    def save_all_data(self, data):
+        with self.pg_conn.cursor() as curs:
+            columns = self.pg_table.columns.split(',')
+            placeholder = '\t'.join('%s' for _ in range(len(columns)))
+            
+            for value in data:
+                print(value)
+                args = ''.join(f'{placeholder}\n' % item for item in value)
+                print(args)
+                data_file = io.StringIO()
+                data_file.write(args)
+                data_file.seek(0)
+                curs.copy_from(data_file, self.pg_table.table, sep='\t', null='None', columns=columns)
